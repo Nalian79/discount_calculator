703ee94c3c9ecbc54c5c17460aeb44990fb07104
Michele <michele.morelock@gmail.com>
2014-12-03 15:37:33 +0000

    Remove unnecessary float()'s from script.

--- discount_calculator.py
+++ discount_calculator.py
@@ -12,17 +12,17 @@ def calculate_discount(item_cost, relative_discount, absolute_discount):
     absolute_discount = float(absolute_discount)
     if item_cost <= 0 or relative_discount < 0 or absolute_discount < 0:
         raise ValueError("Negative and zero numbers are not valid")
-    cost = float(item_cost)
+    cost = item_cost
     logging.debug("Turning item_cost into a float: {!r}".format(cost))
     # Steepest discount we will give is 90%
     lowest_cost = cost - (cost * (90.0/100.0))
     logging.debug("Lowest posible cost for item is: {!r}".format(lowest_cost))
 
-    cost = cost - (cost * (float(relative_discount)/100.0 ))
+    cost = cost - (cost * (relative_discount/100.0))
 
     logging.debug("After the relative discount, cost is: {!r}".format(cost))
     if not cost <= lowest_cost:
-        final_cost = cost - float(absolute_discount)
+        final_cost = cost - absolute_discount
     else:
         print("Item discount is at 90% already!")
         return lowest_cost
